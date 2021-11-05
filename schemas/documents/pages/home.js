1cd83da0890f1e62b235d018c71ff4af72b9e5a2
Flávio Neto <flavioneto96@gmail.com>
2021-11-05 16:18:35 -0300

    seção funcionarios

--- schemas/documents/pages/home.js
+++ schemas/documents/pages/home.js
@@ -69,5 +69,44 @@ export default {
       name: "destinationsButtonText",
       type: "string",
     },
+
+    {
+      title: "Título da seção Equipe",
+      name: "teamTitle",
+      type: "string",
+    },
+    // {
+    //   title: "Texto da seção Destinos",
+    //   name: "destinationsText",
+    //   type: "textContent",
+    // },
+    {
+      title: 'Funcionários',
+      name: 'employees',
+      type: 'array',
+      of: [
+        {
+          title: 'Funcionário',
+          type: 'object',
+          fields: [
+            {
+              title: 'Nome',
+              name: 'name',
+              type: 'string'
+            },
+            {
+              title: 'Função',
+              name: 'position',
+              type: 'string'
+            },
+            {
+              title: 'Foto',
+              name: 'photo',
+              type: 'image'
+            },
+          ]
+        }
+      ]
+    },
   ]
 }
